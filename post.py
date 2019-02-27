933146eae2f8a71acddeb0066e26785eb1c1903c
Ultan-kearns <ultankearns@gmail.com>
2019-02-27 00:57:17 +0000

    Got prefix to postfix working

--- post.py
+++ post.py
@@ -6,13 +6,13 @@ def parse(regex):
 	#check string for operators
 	for i in range (stringLength):
 		if(regex[i] == '|' or regex[i] == '.' or regex[i] == '*'):
-			print(regex[i])
-			operators += regex[i]
-			#replace operator with blank
-			regex = regex.replace(regex[i], " ")
+			operators += regex[i] + " "
+			#replace operator with useless character
+			regex = regex.replace(regex[i], "a", 1)
 	#need to reorganize operators by precedence
 	#add operators to end of expression
 	print ( "BEFORE " + regex)
 	print ("OPERATOR " + operators)
 	regex += operators;		
+	regex = regex.replace("a","")
 	return regex
--- regex.py
+++ regex.py
@@ -1,9 +1,9 @@
-import parse;
+import post;
 
 #infinite while
 while True:
 	#take user regex in and search
 	print("Enter regex expression")
 	regex = input()
-	infix = parse.parse(regex)
+	infix = post.parse(regex)
 	print("INFIX " + infix)
