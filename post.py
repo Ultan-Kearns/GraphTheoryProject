c00f0587af129eb66bfda0dfe76f7d64c6d44133
Ultan-kearns <ultankearns@gmail.com>
2019-02-27 19:27:42 +0000

    partially working

--- post.py
+++ post.py
@@ -6,13 +6,20 @@ def parse(regex):
 	#check string for operators
 	for i in range (stringLength):
 		if(regex[i] == '|' or regex[i] == '.' or regex[i] == '*'):
-			operators += regex[i] + " "
-			#replace operator with useless character
-			regex = regex.replace(regex[i], "a", 1)
+			for i in range(stringLength):				
+				#check to see if any numbers or operators precede operator
+				if(regex[i - 1] != '1' and regex[i - 1 != '0'] and regex[i - 1] != '|' 
+					and regex[i - 1] != '.' and regex[i - 1] != '*'):
+					print("IN " + regex[i + 1])
+					operators += regex[i] + " "
+					regex = regex.replace(regex[i], "a", 1) 
+					#check if next occuring is char is number
+					if(regex[i + 1] == '1' or regex[i + 1] == '0'):
+						regex += operators
+						oprators = ""
 	#need to reorganize operators by precedence
 	#add operators to end of expression
 	print ( "BEFORE " + regex)
-	print ("OPERATOR " + operators)
-	regex += operators;		
+	print ("OPERATOR " + operators)	
 	regex = regex.replace("a","")
 	return regex
