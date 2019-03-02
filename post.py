43d01df80ab1930a49a7a869cdb8682ba0f4298c
Ultan-kearns <ultankearns@gmail.com>
2019-03-02 00:08:08 +0000

    Deciedeed to try putting parentheses around expressions

--- post.py
+++ post.py
@@ -4,15 +4,17 @@ def parse(regex):
 	operators = ""
 	print(regex)
 	#check string for operators
+	#problem is since j is restarting from beginning of string the whole time it puts at end
 	for i in range (stringLength):
 		if(regex[i] == '|' or regex[i] == '.' or regex[i] == '*'):
 			operators += regex[i]
-			regex = regex.replace(regex[i],"",1)
+			regex.replace(regex[i], "",1)
 			for j in range(stringLength):
-				if(regex[j] != 1 or regex[j] != 0 or regex[j] == '|' or regex[j] == '.' or regex[j] == '*'):
-					regex += operators
+				if(regex[j] != "0" or regex[j] != "1" or regex[j] != " "):
+					regex = regex.replace(regex [j],"(",1)
+					regex += ")" + operators
 					operators = " "
-					break;
+					break 
 	#need to reorganize operators by precedence
 	#add operators to end of expression
 	print ( "BEFORE " + regex)
