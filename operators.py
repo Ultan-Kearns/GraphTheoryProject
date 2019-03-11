54dfaf382906481addfc8df3b6f4783d7155fe40
Ultan-kearns <ultankearns@gmail.com>
2019-03-11 17:27:21 +0000

    Improved logic for detecting operators, also found bug when a string such as *.10 is entered the leftmost operator is not moved to end

--- operators.py
+++ operators.py
@@ -9,29 +9,37 @@ def op(regex):
 		#each operation executing more times if another operation is found 
 		#fix this by recording position of last known operator and starting from there
 
-		#kleene returns result for 0 or more 1s or 0s detecting blank spaces for now
+		#kleene returns result for 0 or more 1s or 0s detecting blank spaces for now 
 		#since I count each space as an abscence of 1 and 0
 		if(regex[c] == '*'):
 			print("Kleene")
+			print(prevOpLoc)
 			i = prevOpLoc
-			if(prevOpLoc > 0 and  regex[i - 1] != '|' or regex[i - 1] != '.' or regex[i - 1] != '*'):
-				for i in regex:
-					if(i == '1' or i == '0' or i == '' or i == ' '):
+			if(prevOpLoc > 0 and  regex[c - 1] != '|' or regex[c - 1] != '.' or regex[c - 1] != '*'):
+				for char in range(i,len(regex)):
+					print("regex",regex[char])
+					if(regex[char] == '*'):
+						break
+					elif(regex[char] == '1' or regex[char] == '0' or regex[char] == '' or regex[char] == ' '):
 						kleeneOp+=1
 			else:
 				for i in regex:
+					print("regex",regex[i])
 					if(i == '1' or i == '0' or i == '' or i == ' '):
 						kleeneOp+=1
+					elif(regex[i] == '*'):
+						break;
 			prevOpLoc = c;
 			print("No. of occurrences result of Kleene operation: ",kleeneOp);
 		#this returns a number > 0 if the string contains a 1 followed by a 0 does not take spaces into account
+		#fix this one same way as other two
 		elif(regex[c] == '.'):
 			print("Concatenate")
 			i = prevOpLoc
-			#check to see if a previous operation was performed
-			if(prevOpLoc > 0 and regex[i - 1] != '*' and regex[i - 1] != '|') or regex[i - 1] != '.' or regex[i - 1] != '1' or regex[i - 1] != '0':
-				for i in range(len(regex)):
-					if(regex[i] == '0' and regex[i + 1] == '1' or regex[i] == '1' and regex[i + 1] == '0'):
+			#check to see if a previous operation was performed also need to check which chars to concatenate
+			if(prevOpLoc > 0 and regex[c - 1] != '*' and regex[c - 1] != '|' and regex[c - 1] != '.'):
+				for char in range(i,len(regex)):
+					if(regex[char] == '0' and regex[char + 1] == '1' or regex[char] == '1' and regex[char + 1] == '0'):
 						conOp+=1
 			else:
 				for i in range(len(regex)):
@@ -40,12 +48,10 @@ def op(regex):
 			prevOpLoc = c
 			print("No. of occurrences result of Concatenate operation: ",conOp);
 		elif(regex[c] == '|'):
-			print("regex ")
 			i = prevOpLoc
-			print(prevOpLoc)
+			print("PREVIOUS " ,regex[c-1])
 			#not working if or is after operator
-			if (i > 0 and regex[i - 1] != '*' and regex[i - 1] != '.' and regex[i - 1] != '|' or regex[i-1] != '0' or regex[i - 1] != '1'):
-				print("INSIDE",regex[i],len(regex))
+			if (i > 0 and regex[c - 1] != '*' and regex[c - 1] != '.' and regex[c - 1] != '|'):
 				for char in range(i,len(regex)):
 					if(regex[char] == '0' or regex[char] == '1'):
 						orOp+=1
