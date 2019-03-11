58929e50df4ab73cb0570749c03c916957220fa0
Ultan-kearns <ultankearns@gmail.com>
2019-03-11 16:57:03 +0000

    Added new logic need to improve

--- operators.py
+++ operators.py
@@ -13,9 +13,8 @@ def op(regex):
 		#since I count each space as an abscence of 1 and 0
 		if(regex[c] == '*'):
 			print("Kleene")
-			prevOpLoc = c;
-			if(prevOpLoc > 0 and regex[c - 1] != '0' and regex[c - 1] != '1'):
-				i = prevOpLoc
+			i = prevOpLoc
+			if(prevOpLoc > 0 and  regex[i - 1] != '|' or regex[i - 1] != '.' or regex[i - 1] != '*'):
 				for i in regex:
 					if(i == '1' or i == '0' or i == '' or i == ' '):
 						kleeneOp+=1
@@ -23,14 +22,14 @@ def op(regex):
 				for i in regex:
 					if(i == '1' or i == '0' or i == '' or i == ' '):
 						kleeneOp+=1
+			prevOpLoc = c;
 			print("No. of occurrences result of Kleene operation: ",kleeneOp);
 		#this returns a number > 0 if the string contains a 1 followed by a 0 does not take spaces into account
 		elif(regex[c] == '.'):
 			print("Concatenate")
-			prevOpLoc = c;
+			i = prevOpLoc
 			#check to see if a previous operation was performed
-			if(prevOpLoc > 0 and regex[c - 1] != '0' and regex[c - 1] != '1'):
-				i = prevOpLoc
+			if(prevOpLoc > 0 and regex[i - 1] != '*' and regex[i - 1] != '|') or regex[i - 1] != '.' or regex[i - 1] != '1' or regex[i - 1] != '0':
 				for i in range(len(regex)):
 					if(regex[i] == '0' and regex[i + 1] == '1' or regex[i] == '1' and regex[i + 1] == '0'):
 						conOp+=1
@@ -38,23 +37,24 @@ def op(regex):
 				for i in range(len(regex)):
 					if(regex[i] == '0' and regex[i + 1] == '1' or regex[i] == '1' and regex[i + 1] == '0'):
 						conOp+=1
+			prevOpLoc = c
 			print("No. of occurrences result of Concatenate operation: ",conOp);
 		elif(regex[c] == '|'):
-			print("or") 
-			prevOpLoc = c;
+			print("regex ")
+			i = prevOpLoc
 			print(prevOpLoc)
-			print("REGEX" + regex[c - 1])
-			if(prevOpLoc > 0 and regex[c - 1] != '0' and regex[c - 1] != '1'):
-				print("IN")
-				i = prevOpLoc;
-				for i in range(len(regex)):
-					if(i == '0' or i == '1'):
+			#not working if or is after operator
+			if (i > 0 and regex[i - 1] != '*' and regex[i - 1] != '.' and regex[i - 1] != '|' or regex[i-1] != '0' or regex[i - 1] != '1'):
+				print("INSIDE",regex[i],len(regex))
+				for char in range(i,len(regex)):
+					if(regex[char] == '0' or regex[char] == '1'):
 						orOp+=1
 			else:
 				print("in else")
 				for i in regex:
 					if(i == '0' or i == '1'):
 						orOp+=1
+			prevOpLoc = c;
 			print("No. of occurrences result of or operation: ",orOp);
 		else:
 			print("")
